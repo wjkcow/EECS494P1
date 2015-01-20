a437ec3f939e34568b4924e91a1cfa8e3a14b59b
Jingkui Wang <wjkcow@gmail.com>
2015-01-19 22:25:15 -0500

    anim almost done

--- P1Castlevania/Assets/Scripts/HeroState.cs
+++ P1Castlevania/Assets/Scripts/HeroState.cs
@@ -2,27 +2,50 @@
 using System.Collections;
 
 public class HeroState : MonoBehaviour {
+	private Animator anim;
+	private bool facingLeft = true;
 	// Use this for initialization
 	void Start () {
-
-	
+		anim = GetComponent<Animator> ();
 	}
+
 
 	// Update is called once per frame
 	void Update () {
-		//		Vector3 pos = this.transform.position;
-		//		pos.x = pos.x - walk_speed / 50;
-		//		this.transform.position = pos;
-		//		if (walk_c-- == 0) {
-		//
-		//			if (this.GetComponent<SpriteRenderer> ().sprite == walk_1) {
-		//				this.GetComponent<SpriteRenderer> ().sprite = walk_2;
-		//			} else if (this.GetComponent<SpriteRenderer> ().sprite == walk_2) {
-		//				this.GetComponent<SpriteRenderer> ().sprite = walk_3;
-		//			} else {
-		//				this.GetComponent<SpriteRenderer> ().sprite = walk_1;
-		//			}
-		//			walk_c = walk_frame;
-		//		}
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
+				anim.SetBool("Walk", true);
+		
+		}	
+		if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
+		//	anim.SetBool("Walk", false);
+		}	
+		if(Input.GetKeyDown(KeyCode.RightArrow)) {
+
+
+		}	
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetTrigger("Jump");
+		}
+	}
+
+	void setFaceLeft(bool left){
+		if (left) {
+			if (!facingLeft) {
+				flip();
+				facingLeft = true;
+			}	
+		} else { 
+			if(facingLeft){
+				flip();
+				facingLeft = false;
+			}
+				
+		}
+	}
+	void flip(){
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
---
+++ P1Castlevania/Assets/Scripts/Hero_BaseMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero_BaseMovement : MonoBehaviour {
+	public Vector3 jumpSpeed = new Vector3 (0,1.75f,0);
+	public Vector3 rightSpeed = new Vector3 (0.8f, 0, 0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Gravity g = GetComponent<Gravity> ();		
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			g.setSpeed (jumpSpeed);
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			g.setSpeed (rightSpeed);
+			
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			g.setSpeed (new Vector3 (-1.0f, 0, 0));
+		} 
+
+	}
+}
