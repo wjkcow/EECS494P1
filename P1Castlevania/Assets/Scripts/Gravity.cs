bbf3c7f3f56f7ad2dc6a30b7896fdcb8710f8d4b
Jingkui Wang <wjkcow@gmail.com>
2015-01-20 02:49:41 -0500

    merge

--- P1Castlevania/Assets/Scripts/Gravity.cs
+++ P1Castlevania/Assets/Scripts/Gravity.cs
@@ -60,14 +60,9 @@ public class Gravity : MonoBehaviour {
 	}
 
 	void OnTriggerExit2D(Collider2D other){
-<<<<<<< HEAD
-		if (other.tag == "Ground") {
-			if(other.transform.position.y < transform.position.y + 0.006){
-=======
 		Hero h = GetComponent<Hero> ();
 		if (other.tag == "Ground") {
 			if(other.transform.position.y < transform.position.y && !h.isStairMode){
->>>>>>> 5a6f0714a3983fba09354f6a751cdf93ab0e9c35
 				acc =  Vector3.zero;
 			}
 		} 
