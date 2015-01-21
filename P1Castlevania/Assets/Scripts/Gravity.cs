17799c6ce74ce2c2b51d7eebbf5cad74549333a5
Jingkui Wang <wjkcow@gmail.com>
2015-01-20 22:15:11 -0500

    gravity

--- P1Castlevania/Assets/Scripts/Gravity.cs
+++ P1Castlevania/Assets/Scripts/Gravity.cs
@@ -1,89 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gravity : MonoBehaviour {
+public class Gravity : MonoBehaviour
+{
 
-	public Vector3 g = new Vector3(0,-6f,0);
-	public bool still  	 = false;
-	public bool landByYourself = false;
-	public Vector3 speed = Vector3.zero;
-	public Vector3 acc = Vector3.zero;
-	public float landDelta = 0.0105f;
+		public Vector3 g = new Vector3 (0, -6f, 0);
+		public bool still = false;
+		public bool landByYourself = false;
+		public Vector3 speed = Vector3.zero;
+		public Vector3 acc = Vector3.zero;
+		public float landDelta = 0.0105f;
 
 	
-	// Use this for in =itialization
-	void Start () {
+		// Use this for in =itialization
+		void Start ()
+		{
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+		}
 
-	public void setAcc(Vector3 newAcc){
+		public void setAcc (Vector3 newAcc)
+		{
 
-		this.acc = newAcc;
-	}
-	public void setSpeed(Vector3 newSpeed){
+				this.acc = newAcc;
+		}
 
-		this.speed = newSpeed;
-		FixedUpdate ();
-	}
+		public void setSpeed (Vector3 newSpeed)
+		{
 
-	void FixedUpdate(){
-		if (still) return;
-		float dt = Time.fixedDeltaTime;
+				this.speed = newSpeed;
+				FixedUpdate ();
+		}
 
-		Vector3 curPos = this.transform.position;
-		Vector3 curAcc = acc + g;
-		speed = speed + dt * curAcc;
+		void FixedUpdate ()
+		{
+				if (still)
+						return;
+				float dt = Time.fixedDeltaTime;
 
-		Vector3 nextPos = curPos + speed * dt;
-		rigidbody2D.MovePosition (nextPos);
+				Vector3 curPos = this.transform.position;
+				Vector3 curAcc = acc + g;
+				speed = speed + dt * curAcc;
+
+				Vector3 nextPos = curPos + speed * dt;
+				rigidbody2D.MovePosition (nextPos);
 			
 		
-	}
-	void OnTriggerStay2D(Collider2D other){
-		OnTriggerEnter2D (other);
-	}
-	void OnTriggerEnter2D(Collider2D other){
-<<<<<<< HEAD
-		//print (other.name);
-		Hero h = GetComponent<Hero> ();
-=======
-		if (still|| landByYourself)
-			return;
+		}
 
->>>>>>> FETCH_HEAD
-		if (other.tag == "Ground") {
+		void OnTriggerStay2D (Collider2D other)
+		{
+				OnTriggerEnter2D (other);
+		}
+
+		void OnTriggerEnter2D (Collider2D other)
+		{
+				if (still || landByYourself)
+						return;
+
+				if (other.tag == "Ground") {
 //			print ("hit ground" + other.transform.position.y);
 //			print (transform.position.y);
-			if(other.transform.position.y < transform.position.y  + 0.07){
-				speed = new Vector3(0,0,0);
-				acc = -1 * g;
-				Vector3 pos = transform.position;
-				pos.y = other.transform.position.y + landDelta; // measured
-				transform.position = pos;
-			}
-		} 
-	}
-
-	void OnTriggerExit2D(Collider2D other){
-<<<<<<< HEAD
-		Hero h = GetComponent<Hero> ();
-		if (other.tag == "Ground") {
-			if(other.transform.position.y < transform.position.y  + 0.006 && !h.isStairMode){
-=======
-		if (still || landByYourself) {
-			return;
+						if (other.transform.position.y < transform.position.y + 0.07) {
+								speed = new Vector3 (0, 0, 0);
+								acc = -1 * g;
+								Vector3 pos = transform.position;
+								pos.y = other.transform.position.y + landDelta; // measured
+								transform.position = pos;
+						}
+				} 
 		}
-		if (other.tag == "Ground") {
-			if(other.transform.position.y < transform.position.y){
->>>>>>> FETCH_HEAD
-				acc =  Vector3.zero;
-			}
-		} 
-	}
+
+		void OnTriggerExit2D (Collider2D other)
+		{
+				if (still || landByYourself) {
+						return;
+				}
+				if (other.tag == "Ground") {
+						if (other.transform.position.y < transform.position.y) {
+								acc = Vector3.zero;
+						}
+				} 
+		}
 
 
 }
