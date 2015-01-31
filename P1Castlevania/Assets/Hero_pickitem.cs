64848e1f20e512207b54cfdc8f654efd94cb50c6
lslsheng <527873752@qq.com>
2015-01-30 19:17:47 -0500

    update

--- P1Castlevania/Assets/Hero_pickitem.cs
+++ P1Castlevania/Assets/Hero_pickitem.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class Hero_pickitem : MonoBehaviour {
-
+	public GameObject canvas;
+	private GlobalV globalV;
 	// Use this for initialization
 	void Start () {
-	
+		globalV = canvas.GetComponent<GlobalV> ();
 	}
 	
 	// Update is called once per frame
@@ -15,7 +16,10 @@ public class Hero_pickitem : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.tag == "Pickable") {
 			string k = other.gameObject.GetComponent<Pickable>().kind;
-			print (k + " is picked");
+			print (k + " is picked   " + this.tag + this.name);
+			if (other.name == "Heart(Clone)")
+				globalV.hearts ++;
+
 		}
 	}
 }
