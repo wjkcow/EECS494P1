520beb2d52bd10d5c218233294884187129c7ce5
lslsheng <527873752@qq.com>
2015-01-31 01:00:24 -0500

    UI done, stair fixed, upstair animation still weird

--- P1Castlevania/Assets/Scripts/GlobalV.cs
+++ P1Castlevania/Assets/Scripts/GlobalV.cs
@@ -7,8 +7,8 @@ public class GlobalV : MonoBehaviour {
 	public int score = 0;
 	public int time = 270;
 	public int stage = 1;
-	public int playerLife = 15;
-	public int enemyLife = 15;
+	public int playerLife = 16;
+	public int enemyLife = 16;
 	public int hearts = 0;
 	public int P = 1;
 	public GameObject heartT;
@@ -16,9 +16,11 @@ public class GlobalV : MonoBehaviour {
 	public GameObject scoreT;
 	public GameObject timeT;
 	public GameObject stageT;
-	public GameObject playerT;
-	public GameObject enemyT;
+	public GameObject playerImage;
+	public GameObject enemyImage;
 	public float mytime;
+	public Sprite[] Phealth;
+	public Sprite[] Ehealth;
 
 
 	// Use this for initialization
@@ -28,23 +30,29 @@ public class GlobalV : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (playerLife <= 0) {
+		if (playerLife < 0) {
 			P --;
 			print ("start over");
 		}
 		if (P == 0) {
 			print ("Game Over");		
 		}
-		PT.GetComponent<Text> ().text = String.Concat("--- " , P);
-		heartT.GetComponent<Text> ().text = String.Concat("--- " , hearts);
-		scoreT.GetComponent<Text> ().text = String.Concat("SCORE --- " , score);
-		timeT.GetComponent<Text> ().text = String.Concat("TIME   " , time);
-		stageT.GetComponent<Text> ().text = String.Concat("STAGE   " , stage);
-		playerT.GetComponent<Text> ().text = String.Concat("PLAYER    " , playerLife);
-		enemyT.GetComponent<Text> ().text = String.Concat("ENEMY    " , enemyLife);
-		if (mytime + 1 < Time.time) {
+		PT.GetComponent<Text> ().text = String.Concat("" , P);
+		heartT.GetComponent<Text> ().text = String.Concat("" , hearts);
+		scoreT.GetComponent<Text> ().text = String.Concat("" , score);
+		timeT.GetComponent<Text> ().text = String.Concat("" , time);
+		stageT.GetComponent<Text> ().text = String.Concat("" , stage);
+		playerImage.GetComponent<Image> ().sprite = Phealth [playerLife];
+		enemyImage.GetComponent<Image> ().sprite = Ehealth [enemyLife];
+ 		if (mytime + 1 < Time.time) {
 			mytime = Time.time;	
 			time --;
 		}
+	}
+
+	void updatePlayer(){
+
+
+
 	}
 }
