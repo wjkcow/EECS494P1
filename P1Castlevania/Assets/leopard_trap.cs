133b53ea98103ebcb7e0308e7d47a2a8c595fedb
Jingkui Wang <wjkcow@gmail.com>
2015-02-02 01:21:14 -0500

    call it a day

---
+++ P1Castlevania/Assets/leopard_trap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class leopard_trap : MonoBehaviour {
+	public GameObject l;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+
+		if (other.tag == "Hero") {
+			l.GetComponent<Leopard>().start = true;
+		} 
+	}
+}
