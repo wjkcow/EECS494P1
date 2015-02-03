920a1d371b3edcb1c5f1f7db045ee608e72c8fff
Jingkui Wang <wjkcow@gmail.com>
2015-02-02 22:17:33 -0500

    working on custom level

---
+++ P1Castlevania/Assets/trapTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trapTrigger : MonoBehaviour {
+	public GameObject pull_green;	
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Hero") {
+			pull_green.GetComponent<pull_green>().triggering();
+		}
+	}
+}
