75338e8d15a82bb30dac6d93ca9e32362d0852a6
Jingkui Wang <wjkcow@gmail.com>
2015-02-03 23:24:27 -0500

    working on custom

---
+++ P1Castlevania/Assets/elevator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class elevator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+
+	
+	}
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (other.tag == "Hero") {
+			print ("in");
+			Vector3 pos = other.transform.position;
+			pos.y = transform.position.y;
+			other.transform.position = pos;
+		}
+	}
+}
